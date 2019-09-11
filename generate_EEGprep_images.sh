575e977436577809b1b97c78b896b53c619d6488
PeerHerholz <herholz.peer@gmail.com>
2019-09-11 09:52:20 +0200

    update image build
    - include option to build images locally

--- generate_EEGprep_images.sh
+++ generate_EEGprep_images.sh
@@ -25,13 +25,31 @@ generate_singularity() {
             --miniconda \
                conda_install="python=3.6 numpy nipype nibabel pandas mne" \
                create_env='eegprep' \
-               activate=true 
+               activate=true
 }
 
 # generate files
 generate_docker > Dockerfile
 generate_singularity > Singularity
 
-# Build images using the saved files
-#docker build -t eegprep:test .
-#singularity build eegprep.simg Singularity
+# check if images should be build locally or not
+if [ '$1' = 'local' ]; then
+  if [ '$2' = 'docker' ]; then
+    echo "docker image will be build locally"
+    # build image using the saved files
+    docker build -t eegprep:test .
+  elif [ '$2' = 'singularity']; then
+    echo "singularity image will be build locally"
+    # build image using the saved files
+    singularity build eegprep.simg Singularity
+  elif [ '$2' = 'both' ]; then
+    echo "docker and singularity images will be build locally"
+    # build images using the saved files
+    docker build -t eegprep:test .
+    singularity build eegprep.simg Singularity
+  elif [ -z "$2" ]; then
+    echo "Please indicate which image should be build. You can choose from docker, singularity or both."
+  fi
+else
+  echo "Image(s) won't be build locally."
+fi
