d22a0465a3e96331856996114bf6b51d1329721e
Peer Herholz <herholz.peer@gmail.com>
2020-04-16 16:38:39 -0400

    Create utils.py

---
+++ eegprep/utils/utils.py
@@ -0,0 +1,13 @@
+
+
+def validate_input_dir(input_dir):
+
+
+
+def check_fif(input, output, keep_fif=False):
+
+
+    if keep_if = True:
+      save_file(opj(input_dir, derivatives, filename)
+
+    return data_fif
